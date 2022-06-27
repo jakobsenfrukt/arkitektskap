d842163c65ac54307a28aba7af007c89f7185a58
elisejakob <hei@elisejakob.no>
2022-06-27 12:10:58 +0200

    add numbers to about page

--- web/src/pages/Om.vue
+++ web/src/pages/Om.vue
@@ -1,6 +1,31 @@
 <template>
   <Layout>
     <p class="intro about-intro" v-html="$replaceLogo($page.about.intro)"></p>
+    <ul class="about-numbers">
+      <h2><strong>arkitektskap</strong> i tall</h2>
+      <li>
+        <span class="number">{{ $page.projects.edges.length }}</span> prosjekter
+      </li>
+      <li>
+        over <span class="number">{{ getActiveYears() }}</span> år
+      </li>
+      <li>
+        i <span class="number">{{ $page.locations.edges.length }}</span> byer
+      </li>
+      <li>
+        for <span class="number">{{ $page.clients.edges.length }}</span> ulike
+        oppdragsgivere
+      </li>
+      <li>
+        nå er vi
+        <span class="number">{{ $page.people.edges.length }}</span> folk
+      </li>
+      <li>
+        som samarbeider på tvers med
+        <span class="number">{{ $page.collaborators.edges.length }}</span>
+        samarbeidspartnere
+      </li>
+    </ul>
     <div class="about-content">
       <BlockContent
         :blocks="$page.about._rawBody"
@@ -17,6 +42,41 @@ query {
     intro
     _rawBody
   }
+  projects: allSanityProject(sortBy: "year") {
+    edges {
+      node {
+        id
+      }
+    }
+  }
+  people: allSanityPerson(sortBy: "name") {
+    edges {
+      node {
+        id
+      }
+    }
+  }
+  locations: allSanityLocation(sortBy: "title") {
+    edges {
+      node {
+        id
+      }
+    }
+  }
+  clients: allSanityClient(sortBy: "title") {
+    edges {
+      node {
+        id
+      }
+    }
+  }
+  collaborators: allSanityCollaborator(sortBy: "title") {
+    edges {
+      node {
+        id
+      }
+    }
+  }
 }
 </page-query>
 
@@ -26,6 +86,14 @@ import BlockContent from "~/components/tools/BlockContent";
 export default {
   components: {
     BlockContent,
+  },
+  methods: {
+    getActiveYears() {
+      let now = new Date(Date.now());
+      let startDate = new Date(1985, 12, 3);
+      let yearsDiff = now.getFullYear() - startDate.getFullYear();
+      return yearsDiff;
+    },
   },
   metaInfo: {
     title: "Om",
@@ -46,5 +114,20 @@ export default {
 }
 .about-content {
   grid-column: 1 / -1;
+}
+.about-numbers {
+  grid-column: 1 / -1;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: grid;
+
+  h2 {
+    grid-column: 1 / -1;
+  }
+  .number {
+    font-family: var(--font-mono);
+    font-size: var(--font-size-xl);
+  }
 }
 </style>
